6fa08e810887d5181276d50b7e947fb8efd0007f
cameronnh <gasdrawls@users.noreply.github.com>
2021-11-22 16:23:18 -0500

    dropper fix/ friends database init

---
+++ GeoBD/dbo/Tables/friendship.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[friendship]
+(
+	[requesterId] INT NOT NULL, 
+    [addresseeId] INT NOT NULL, 
+    CONSTRAINT Friendship_PK            PRIMARY KEY (RequesterId, AddresseeId),
+    CONSTRAINT FriendshipToRequester_FK FOREIGN KEY (RequesterId)
+        REFERENCES [dbo].[user] (userId),
+    CONSTRAINT FriendshipToAddressee_FK FOREIGN KEY (AddresseeId)
+        REFERENCES [dbo].[user] (userId)
+)
